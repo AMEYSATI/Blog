01155ea8d4c06b116a8072b19a88f53d00d0e149
AMEYSATI <78717195+AMEYSATI@users.noreply.github.com>
2024-06-27 01:18:25 +0530

    Update Register.jsx

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -24,7 +24,7 @@ function Register() {
                 { withCredentials: true } // Ensure cookies are included
             );
             if (response.status === 200) {
-                navigate('/home');
+                 await axios.get('https://blog-backend-khj7.onrender.com/home', { withCredentials: true });
             }
         } catch (error) {
             if (error.response && error.response.status === 400) {
