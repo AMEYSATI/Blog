7d8f483927d86278f16b81f0c05590baace77770
AMEYSATI <78717195+AMEYSATI@users.noreply.github.com>
2024-06-27 16:44:40 +0530

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -24,14 +24,7 @@ function Login() {
             { withCredentials: true }); 
 
             if (response.status === 200) {
-                console.log("Login successful. Cookie set:", document.cookie);
-                // Make a GET request to /home to ensure the session is established
-                const homeResponse = await axios.get('https://blog-backend-khj7.onrender.com/home', { withCredentials: true });
-                if (homeResponse.status === 200) {
-                    navigate('/home');
-                } else {
-                    console.error('Failed to access /home:', homeResponse.status);
-                }
+                    navigate('/home');   
             }
 
         } catch (error) {
